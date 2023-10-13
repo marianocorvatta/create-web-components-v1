ab8a6a2642cca66a1ae376342fdb1f4bced67a66
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-10-13 07:50:57 +0200

    add icons and some style

--- src/app/_components/CodeEditor/index.tsx
+++ src/app/_components/CodeEditor/index.tsx
@@ -1,6 +1,10 @@
 'use client'
 
+import { useState } from 'react';
 import Editor from '@monaco-editor/react';
+import styles from './CodeEditor.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faCss3, faJs } from '@fortawesome/free-brands-svg-icons'
 
 const DEFAULT_COMPONENT = `
 function MiComponente(props) {
@@ -14,10 +18,51 @@ function MiComponente(props) {
 export default MiComponente;
 `;
 
+const files = {
+	'Component.jsx': {
+	  name: 'Component.jsx',
+	  language: 'javascript',
+	  value: DEFAULT_COMPONENT,
+	},
+	'style.css': {
+	  name: 'style.css',
+	  language: 'css',
+	  value: "someCSSCodeExample",
+	},
+	'index.html': {
+	  name: 'index.html',
+	  language: 'html',
+	  value: "someHTMLCodeExample",
+	},
+  } as const;
+
+
 const CodeEditor = () => {
+	const [fileName, setFileName] = useState('Component.jsx');
+	const file = files[fileName];
+
   return (
-    <Editor height="100vh" defaultLanguage="javascript" defaultValue={DEFAULT_COMPONENT} />
-  )
+    <>
+    <div className={styles.topBar}>
+    <button className={styles.htmlButton} disabled={fileName === 'index.html'} onClick={() => setFileName('index.html')}>
+      <div><FontAwesomeIcon icon={faHtml5} /></div>index.html
+      </button>
+      <button className={styles.cssButton} disabled={fileName === 'style.css'} onClick={() => setFileName('style.css')}>
+      <div><FontAwesomeIcon icon={faCss3} /></div>style.css
+      </button>
+      <button className={styles.jsButton} disabled={fileName === 'Component.jsx'} onClick={() => setFileName('script.js')}>
+      <div><FontAwesomeIcon  icon={faJs} /></div> Component.jsx
+      </button>
+    </div>
+    <Editor
+      height="100vh"
+      theme="vs-dark"
+      path={file.name}
+      defaultLanguage={file.language}
+      defaultValue={file.value}
+    />
+  </>
+	)
 }
 
 export default CodeEditor;
